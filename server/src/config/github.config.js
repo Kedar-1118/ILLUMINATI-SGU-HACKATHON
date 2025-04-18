a139c3b3bb011a89cba8119f6026fd559fbcbca1
Onkar Rane <onkarrane60@gmail.com>
2025-04-18 18:42:30 +0530

    refactor GitHub authentication strategy for improved readability and error handling

--- server/src/config/github.config.js
+++ server/src/config/github.config.js
@@ -1,10 +1,9 @@
 import passport from "passport";
-import {Strategy as GitHubStrategy} from "passport-github2";
+import { Strategy as GitHubStrategy } from "passport-github2";
 import { User } from "../models/user.model.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import { AsyncHandler } from "../utils/wrapAsync.js";
-import mongoose from "mongoose";
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((obj, done) => done(null, obj));
@@ -15,30 +14,29 @@ passport.use(
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL: process.env.GITHUB_CALLBACK_URL,
+      scope: ["user:email"],
     },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        let user = await User.findOne({ githubId: profile.id });
+    AsyncHandler(async (accessToken, refreshToken, profile, done) => {
+      let user = await User.findOne({ githubId: profile.id });
 
-        if (!user) {
-          user = new User({
-            login: profile.username,
-            email: profile.emails[0].value,
-            name: profile.displayName,
-            avatar_url: profile.photos[0].value,
-            bio: profile._json.bio,
-            location: profile._json.location,
-            provider: "github",
-            githubId: profile.id,
-          });
-          console.log("User created:", user);
-          await user.save();
-        }
+      console.log("Access Token", accessToken);
+      console.log("Profile", profile);
 
-        return done(null, user);
-      } catch (err) {
-        return done(err, null);
+      if (!user) {
+        user = new User({
+          login: profile.username,
+          email: profile.emails[0]?.value,
+          name: profile.displayName,
+          avatar_url: profile.photos[0]?.value,
+          bio: profile._json.bio,
+          location: profile._json.location,
+          provider: "github",
+          githubId: profile.id,
+        });
+        await user.save();
       }
-    }
+
+      return done(null, user);
+    })
   )
 );
