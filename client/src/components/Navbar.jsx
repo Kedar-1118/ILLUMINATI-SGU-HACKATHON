fd4f0665b7caf39321eb8dbdb3a47990fdc27d14
AlokKulkarni-11 <163656068+AlokKulkarni-11@users.noreply.github.com>
2025-04-20 06:10:52 +0530

    Update Navbar.jsx name update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
 
           {/* Logo / Site Name */}
           <div className="flex-shrink-0 text-xl font-bold bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">
-            OSMatch
+            OpenNest
           </div>
 
           {/* Desktop Menu */}
