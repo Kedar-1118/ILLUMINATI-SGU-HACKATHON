9deb19bf02a12763d499b3c9615a31fccc5ff26f
AlokKulkarni-11 <163656068+AlokKulkarni-11@users.noreply.github.com>
2025-04-20 06:09:19 +0530

    Update Footer.jsx  name update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     <footer className="bg-gray-800 text-gray-300 py-6 ">
   <div className="container mx-auto px-6 text-sm">
     <div className="flex flex-col md:flex-row justify-between items-center">
-      <p>&copy; {new Date().getFullYear()} OpenSource Matchmaker. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} OpenNest . All rights reserved.</p>
       <div className="mt-2 md:mt-0">
-        <span className="block md:inline">Contact: team.opensource@email.com</span>
+        <span className="block md:inline">Contact: team.opennest@gmail.com</span>
         <span className="hidden md:inline mx-2">|</span>
-        <span className="block md:inline">GitHub: github.com/team-open-source</span>
+        <span className="block md:inline">GitHub: github.com/team-open-nest</span>
       </div>
     </div>
   </div>
